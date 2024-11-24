40405729655bcd55f0b8ec3446a4df26faef7f6d
Neodast <valinkevichroma@gmail.com>
2024-11-24 10:41:44 +0200

    AGAIN

--- backend/src/user/tests/user.controller.spec.ts
+++ backend/src/user/tests/user.controller.spec.ts
@@ -26,7 +26,7 @@ describe('User service', () => {
     userController = moduleRef.get<UserController>(UserController);
   });
 
-  describe('findAll', async () => {
+  describe('findAll', () => {
     it('Should return an array of users', async () => {
       const result: UserDto[] = [
         {
